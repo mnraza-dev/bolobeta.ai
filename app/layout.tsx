5237b05d53813a8741ccffb31e6c057804b32fc8
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-06 23:01:29 +0530

    Update React version to 18, enhance sidebar styles with new CSS variables, and integrate sidebar components into layout and home page for improved UI structure.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,9 +24,13 @@ export default function RootLayout({
       <body
         className={`${inter.className} `}
       >
-        <main className="min-h-screen ">
-          {children}
-        </main>
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="min-h-screen ">
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from '@/components/ui/button'
+import { Label } from '@/components/ui/label'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='p-8 flex gap-2'>
+      <Label>
+        Home
+      </Label>
+      <Button variant={"default"} size={"lg"}>
+        Hello
+      </Button>
+    </div>
   )
 }
 
