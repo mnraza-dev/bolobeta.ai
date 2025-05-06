f3b3bea5b5e44016525520e25264fbf2c33b4b3e
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-07 00:08:54 +0530

    Update ChatInputBox styles: modify border radius and input padding for improved aesthetics and user experience.

--- components/ChatInputBox.tsx
+++ components/ChatInputBox.tsx
@@ -18,15 +18,15 @@ const ChatInputBox = () => {
         <span className="text-6xl font-thin">Beta.ai</span>
       </div>
 
-      <div className='max-w-2xl p-2 w-full  border rounded-lg border-gray-600'>
+      <div className='max-w-2xl p-2 w-full border rounded-xl border-gray-600'>
         <Tabs defaultValue="search" className="w-[260px]">
-         
+
           <TabsContent value="search">
-          <Input type='text' className='outline-none p-4 border-none w-full focus:#fff' placeholder='Ask Anything' />
+            <Input type='text' className='outline-none p-14 border-none w-full focus:#fff' placeholder='Ask Anything' />
 
           </TabsContent>
           <TabsContent value="research">
-          <Input type='text' className='outline-none p-4 border-none w-full focus:#fff' placeholder='Research Anything' />
+            <Input type='text' className='outline-none p-4 border-none w-full focus:#fff' placeholder='Research Anything' />
 
           </TabsContent>
           <TabsList className="grid w-full grid-cols-2">
